6094b0f9297682a468a7e0af6c7f4c4f4ce25902
Cristian Nass <cristian.tirdad@gmail.com>
2024-03-06 18:03:43 +0100

    Implemented create Async Thunk

--- src/components/CounterComponent.tsx
+++ src/components/CounterComponent.tsx
@@ -1,14 +1,15 @@
 import { useSelector, useDispatch } from "react-redux";
-import type { RootState } from "../state/store";
+import type { RootState, AppDispatch } from "../state/store";
 import {
   increment,
   decrement,
   incrementByAmount,
+  incrementAsync
 } from "../state/counter/counterSlice";
 
 const CounterComponent = () => {
   const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
 
   const incrementToggle = () => {
     console.log("Increment");
@@ -23,12 +24,17 @@ const CounterComponent = () => {
     dispatch(incrementByAmount(5));
   };
 
+  const incrementAsyncToggle = () => {
+    dispatch(incrementAsync(2));
+  };
+
   return (
     <>
       <div>{count}</div>
       <button onClick={incrementToggle}>+1</button>
       <button onClick={decrementToggle}>-1</button>
       <button onClick={incrementByAmountToggle}>+5</button>
+      <button onClick={incrementAsyncToggle}>+2</button>
     </>
   );
 };
