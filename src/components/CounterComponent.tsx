626078690f1066697d710728d591285cdab15bd8
Cristian Nass <cristian.tirdad@gmail.com>
2024-03-06 17:45:21 +0100

    add increment by 5

--- src/components/CounterComponent.tsx
+++ src/components/CounterComponent.tsx
@@ -1,26 +1,34 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 import type { RootState } from "../state/store";
-import {increment, decrement} from '../state/counter/counterSlice'
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "../state/counter/counterSlice";
 
 const CounterComponent = () => {
   const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const incrementToggle = () => {
-    console.log('Increment');
+    console.log("Increment");
     dispatch(increment());
-  }
+  };
 
   const decrementToggle = () => {
     dispatch(decrement());
-  }
+  };
 
+  const incrementByAmountToggle = () => {
+    dispatch(incrementByAmount(5));
+  };
 
   return (
     <>
       <div>{count}</div>
-      <button onClick={incrementToggle}>+</button>
-      <button onClick={decrementToggle}>-</button>
+      <button onClick={incrementToggle}>+1</button>
+      <button onClick={decrementToggle}>-1</button>
+      <button onClick={incrementByAmountToggle}>+5</button>
     </>
   );
 };
