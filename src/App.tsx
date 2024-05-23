bb6e4333f3b072a04d11970e94f4f8465ed789bf
Cristian Nass <cristian.tirdad@gmail.com>
2024-05-23 15:35:59 +0200

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import CounterComponent from "./components/CounterComponent";
+import NavigationBar from "./components/NavigationBar";
 import TodoComponent from "./components/TodoComponent";
 
 function App() {
   return (
     <>
+      <NavigationBar />
       <div>Counter</div>
       <CounterComponent />
       <TodoComponent />
---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useWindowSize } from "usehooks-ts";
+import "../styles/navigation-bar.css";
+
+const NavigationBar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { width } = useWindowSize();
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-logo">Logo</h1>
+      <div
+        className={`hamburger ${width <= 600 ? "visible" : ""}`}
+        onClick={toggleMenu}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className={`nav-links ${width > 600 || isOpen ? "show" : "hide"}`}>
+        <button className="nav-button">Home</button>
+        <button className="nav-button">About</button>
+        <button className="nav-button">Contact</button>
+        <button className="nav-button">News</button>
+      </div>
+    </nav>
+  );
+};
+
+export default NavigationBar;
